7f40c9a9e99b0ca629a70901e39ee449df4b794b
Metee <metee.yingyong@kkumail.com>
2024-06-05 20:29:06 +0700

    add comments in hello tfjs

--- hello/hello_world.js
+++ hello/hello_world.js
@@ -2,13 +2,16 @@
 document.addEventListener('DOMContentLoaded', (_event) => {
 
 	async function doTraining(model) {
-		const totalEpochs = 10;
+		const totalEpochs = 100;
 		const progressBar = document.getElementById('progress-bar');
 		const epochDisplay = document.getElementById('epoch');
 		const lossDisplay = document.getElementById('loss');
 
 		const history = await model.fit(xs, ys, {
 			epochs: totalEpochs,
+			// callbacks is FUNCTION
+			// that invoke everytime other function (`fit` function in this case)
+			// is being invoked
 			callbacks: {
 				onEpochEnd: async (epoch, logs) => {
 					console.log('Epoch: ' + epoch + ' Loss: ' + logs.loss);
@@ -25,18 +28,25 @@ document.addEventListener('DOMContentLoaded', (_event) => {
 
 	const model = tf.sequential();
 	model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
+	// finished define model architecture
 
 	model.compile({
-		loss: 'meanSquaredError',
-		optimizer: 'sgd'
+		loss: 'meanSquaredError', // Python: 'mean_squared_error'
+		optimizer: 'sgd' // 'adam'
 	});
 
+	// model.fit(xs, ys, {epochs: 10})
+
+	// Relationship: y = 2*x - 1
 	const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
 	const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
+
+	// You can do this without resolve then
+	// doTraining(model);
 
 	doTraining(model).then(history => {
 		console.log('Training complete');
 		console.log('Loss:', history.history.loss[0]);
 	});
-	
+
 });
